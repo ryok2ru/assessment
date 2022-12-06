0c58f521760aac81372ec5f1c3fc89e327151ae5
ryok2ru <87634425+ryok2ru@users.noreply.github.com>
2022-12-06 12:32:43 +0900

    bootstrap適応

--- assessment.js
+++ assessment.js
@@ -20,18 +20,31 @@ assessmentButton.onclick = () => {
   if(userName.length === 0){
     resultReset();
     return;
-  };
+};
 
   //  診断結果表示エリアの制作
-  removeAllChildren(resultDivided);
-  const header = document.createElement('h3');
-  header.innerText = '診断結果'
-  resultDivided.appendChild(header);
-  
+  resultDivided.innerText = "";
+  // headerDivided の作成
+  const headerDivided = document.createElement('div');
+  headerDivided.setAttribute('class','card-header');
+  headerDivided.innerText = '診断結果';
+
+  // bodyDivided の作成
+  const bodyDivided = document.createElement('div');
+  bodyDivided.setAttribute('class','card-body');
+
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class','card-text')
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDivided.appendChild(paragraph);
+  bodyDivided.appendChild(paragraph);
+
+  // resultDivided に bootstrap のスタイルを適応する。
+  resultDivided.setAttribute('class','card');
+  resultDivided.setAttribute('style','max-widgh: 700px');
+  // headerDivided と bodyDivided を resultDivided に差し込む。
+  resultDivided.appendChild(headerDivided);
+  resultDivided.appendChild(bodyDivided);
 
   // ツイートエリアの作成
   removeAllChildren(tweetDivided);
